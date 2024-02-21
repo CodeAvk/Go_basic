f3c522d143b9dd8ec1ab92c3e2c602fbb6a13978
Abhisek0770 <abhisek.s@nextbiginnovationslab.com>
2024-02-21 21:31:22 +0530

    Handling Web Requests

---
+++ go_web/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://codeavk.github.io/portfoliobnb.git.io-main/"
+
+func main() {
+	fmt.Println("Go")
+
+	// Create a custom HTTP client that skips certificate verification
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	response, err := client.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+	defer fmt.Printf("Response type is : %T\n", response)
+
+	databyte, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(databyte)
+	println(content)
+
+}
